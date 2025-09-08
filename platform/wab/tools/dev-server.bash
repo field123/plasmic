b17ca7d3423f6dc62df86dd3e8916b2cda9f0ed4
Robert Field <robertfield@quadrimular.com>
2025-09-08 16:03:49 +0100

    build: trying something else

--- platform/wab/tools/dev-server.bash
+++ platform/wab/tools/dev-server.bash
@@ -6,7 +6,7 @@ port=${PORT:-3003}
 if [[ $REACT_APP_DEV_HOST_PROXY ]]; then
   HOST_URL=${REACT_APP_DEV_HOST_PROXY}/static/host.html
 elif [[ $REACT_APP_DEV_PROXY ]]; then
-  HOST_URL=https://host.plasmicdev.com/static/host.html
+  HOST_URL=https://host.plasmicdev.rcf.dev/static/host.html
 else
   HOST_URL=http://localhost:${HOSTSERVER_PORT:-3005}/static/host.html
 fi
