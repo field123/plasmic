757508015451089e7d9f298e572247deca8b1e06
Robert Field <robertfield@quadrimular.com>
2025-09-12 14:41:48 +0100

    feat: devflags hardcoded

--- platform/wab/src/wab/shared/devflags.ts
+++ platform/wab/src/wab/shared/devflags.ts
@@ -283,7 +283,7 @@ const DEFAULT_DEVFLAGS = {
     process.env.CODEGEN_ORIGIN_HOST ||
     process.env.CODEGEN_HOST ||
     "http://codegen-origin.plasmic.app",
-  adminTeamDomain: production ? "plasmic.app" : "admin.example.com",
+  adminTeamDomain: production ? "elasticpath.com" : "admin.example.com",
   defaultHostUrl:
     process.env.REACT_APP_DEFAULT_HOST_URL ||
     "https://host.plasmicdev.rcf.dev/static/host.html",
